ee4195275d2381a67b2a02fc0eaa356ecffa86d7
임광민 <imgwangmin@imgwangmin-ui-MacBookAir.local>
2025-08-27 18:02:05 +0900

    Fix:: 로그아웃 url 수정

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -8,7 +8,7 @@ export default function Dashboard() {
 
   const handleLogout = async () => {
     // 로그아웃 API 호출
-    const response = await fetch('/api/logout');
+    const response = await fetch('/api/auth/logout');
 
     if (response.ok) {
       // 로그아웃 성공 시 로그인 페이지로 이동
